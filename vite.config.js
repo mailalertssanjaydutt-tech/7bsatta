0f31900fd8d41e9235c298a2757405f179c83e33
Aviin <aviinyou07@example.com>
2025-12-06 21:57:22 +0530

    fixed

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // This allows your local dev server to be accessed from any network interface
-    host: true, 
-    
-    // 👇 Add the ngrok host here to explicitly allow access
-    allowedHosts: [
-      '5ed988d72ac4.ngrok-free.app',
-      // If you are using other public hosts or IP addresses, you can add them here
-    ],
-  },
 })
